0a2490202768a8a5358e6ed463d5e9f0b22f59f6
Daniel Amado <64038165+danielAcl9@users.noreply.github.com>
2023-08-29 10:41:47 -0500

    Add files sql prompt

---
+++ Ad.sql
@@ -0,0 +1,5 @@
+INSERT INTO F1_2023(driver_name, driver_abb, driver_rank, driver_points, driver_victories, driver_podiums, driver_poles)
+VALUES 
+    ('M VERSTAPPEN', 'VER', 1, 339, 11, 13, 8), 
+    ('S PEREZ', 'PER', 2, 201, 2, 7, 2), 
+    ('F ALONSO', 'ALO', 3, 168, 0, 7, 0);
